d4f42385f5769d7f52ae970578151f3ab2687936
Lucas Draeger de Andrade <ldg@certi.org.br>
2022-12-06 20:19:16 -0300

    alterações no codigo da interface para receber as atualizações relacionadas ao sensor e ao novo arquivo json

--- projeto_monitoramento/User_Interface/Monitoramento_GUI.py
+++ projeto_monitoramento/User_Interface/Monitoramento_GUI.py
@@ -24,24 +24,24 @@ string_dateTime_temperature_Celcius = (f"{temperatura_sensor_celcius} | {now}")
 string_dateTime_temperature_Farenheit = (f"{temperatura_sensor_farenheit} | {now}") 
 
 # -------------------------------------------------------------------------------------------------------------------------------- 
-
-window = Tk()
-window.geometry('580x160')
-window.title('Projeto Monitoramento')
-
-label = Label(window, text='Projeto Monitoramento')
-label.grid(row=0, column=1, padx=10, pady=10) 
-
-temperature = Label(window, text = "Temperatura Ambiente ºC | Date Time:")
-Ttemperature = Label(window, text= string_dateTime_temperature_Celcius)
-
-temperature.grid(row=1, column=0, padx=10, pady= 10) 
-Ttemperature.grid(row=1, column=1, padx=10, pady= 10)
-
-humidity = Label(window, text = "Temperatura Ambiente ºF | Date Time:")
-Hhumidity = Label(window, text= string_dateTime_temperature_Farenheit)
-
-humidity.grid(row=2, column=0, padx=10, pady= 10) 
-Hhumidity.grid(row=2, column=1, padx=10, pady= 10)
-
-window.mainloop()
+while True: 
+    window = Tk()
+    window.geometry('580x160')
+    window.title('Projeto Monitoramento')
+    
+    label = Label(window, text='Projeto Monitoramento')
+    label.grid(row=0, column=1, padx=10, pady=10) 
+    
+    temperature = Label(window, text = "Temperatura Ambiente ºC | Date Time:")
+    Ttemperature = Label(window, text= string_dateTime_temperature_Celcius)
+    
+    temperature.grid(row=1, column=0, padx=10, pady= 10) 
+    Ttemperature.grid(row=1, column=1, padx=10, pady= 10)
+    
+    humidity = Label(window, text = "Temperatura Ambiente ºF | Date Time:")
+    Hhumidity = Label(window, text= string_dateTime_temperature_Farenheit)
+    
+    humidity.grid(row=2, column=0, padx=10, pady= 10) 
+    Hhumidity.grid(row=2, column=1, padx=10, pady= 10)
+    
+    window.mainloop()
